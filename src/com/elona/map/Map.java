f6fb050bc6f29d25097ffff576416b922a6e7578
Yamanishi <@siroi_kona>
2015-09-09 11:19:07 +0900

    最初のコミット

---
+++ src/com/elona/map/Map.java
@@ -0,0 +1,16 @@
+package com.elona.map;
+
+public class Map {
+    
+    int[][] mapChipID;
+    int mapChipFileID;
+    int floor;
+    
+    public Map(int[][] mcID, int mcfID, int f){
+        mapChipID=mcID;
+        mapChipFileID=mcfID;
+        floor=f;
+        
+    }
+    
+}
